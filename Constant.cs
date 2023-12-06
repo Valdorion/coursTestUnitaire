b2bc1ecfa3f1d213db5f1a3300217de21d28362f
Valdorion <valdorion.kalagia@gmail.com>
2023-12-06 10:26:59 +0100

    first (test)

---
+++ Constant.cs
@@ -0,0 +1,17 @@
+﻿namespace coursTestUnitaire.Constant;
+
+public class En
+{
+    public const string hello = "Hello";
+    public const string goodEvening = "Good evening";
+    public const string bye = "Goodbye";
+    public const string goodSay = "Well said !";
+}
+
+public class Fr
+{
+    public const string hello = "Bonjour";
+    public const string goodEvening = "Bonsoir";
+    public const string bye = "Au revoir";
+    public const string goodSay = "Bien dit !";
+}
---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using coursTestUnitaire.Constant;
+
+
+namespace coursTestUnitaire
+{
+    class Echauffement
+    {
+        const string lang = "En";
+        private static bool boucle = true;
+        
+        static void Main()
+        {
+            string? input = "";
+            string output = "";
+            string hello = "";
+            string goodEvening = "";
+            string bye = "";
+            string goodSay = "";
+            if (lang == "En")
+            {
+                hello = En.hello;
+                goodEvening = En.goodEvening;
+                bye = En.bye;
+                goodSay = En.goodSay;
+            }
+            else if (lang == "Fr")
+            {
+                hello = Fr.hello;
+                goodEvening = Fr.goodEvening;
+                bye = Fr.bye;
+                goodSay = Fr.goodSay;
+            }
+            DateTime now = DateTime.Now;
+            if(now.Hour >= 18)
+            {
+                Console.WriteLine(goodEvening);
+            }
+            else
+            {
+                Console.WriteLine(hello);
+            }
+            while (boucle)
+            {
+                input = Console.ReadLine();
+                if (input==""){input=null;}
+                if (input != null)
+                {
+                    for(int i = input.Length-1; i >= 0; i = i-1)
+                    {
+                        output = output + input[i];
+                    }
+                    if (output == input)
+                    {
+                        Console.WriteLine(goodSay);
+                    }
+                    else
+                    {
+                        Console.WriteLine(output);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine(bye);
+                    boucle = false;
+                }
+                input = "";
+                output = "";
+            }
+        }
+    }
+}
